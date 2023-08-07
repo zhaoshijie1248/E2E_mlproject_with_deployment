e4594e15b941b175269f0bbfad811f561f9797d5
SHIJIE <59632536+zhaoshijie1248@users.noreply.github.com>
2023-08-06 23:10:45 -0700

    update notebook

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ def get_requirements(file_path:str)->List[str]:
     '''
     this function will return the list of requirements
     '''
-
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
